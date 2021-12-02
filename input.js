248b9342b022452ebfab9cf6ee9fce6d0b3a2366
hsaidahmed <hashim.saidahmed@gmail.com>
2021-12-02 01:52:57 +0000

    keys assigned with WASD keys

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function() {
+let conn = 0;
+const setupInput = function(connection) {
+  conn = connection;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,9 +11,21 @@ const setupInput = function() {
 };
 
 const handleUserInput = function(key) {
-  process.stdout.write('.');
+  // process.stdout.write('.');
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    conn.write("Move: up");
+  }
+  if (key === 's') {
+    conn.write("Move: down");
+  }
+  if (key === 'a') {
+    conn.write("Move: left");
+  }
+  if (key === 'd') {
+    conn.write("Move: right");
   }
 };
 module.exports = {setupInput, handleUserInput};
